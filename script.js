e2eb0c1b56f2f691ab3abad1dc386883382533f4
Turbotor <63958625+Turbotor@users.noreply.github.com>
2020-05-31 19:07:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+//Q1
+var petArray=("cat","cow","dog")
+//Q2
+console.log(cow);
+//Q3
+var petArray=("cat","cow","dog")
+petArray.push(sheep);
+//Q4
+var petArray=("cat","cow","dog","sheep")
+console.log(petArray.length); 4
+console.log(petArray)
+[4]
+//Q5
+var CatObject = {
+name: "Fluffy"
+Colour: "brown"
+Age: 6
+}
+//Q6
+console.log(CatObject.Age)
+6
+//Q7
+var CatArray
+CatArray.push("Fluffy","brown",6)
+console.log(CatArray.lenght);
+3
+console.log(CatArray)
+[3]
+//Q8
+for(var count = 3; count <= 3; count++) {
+    console.log(count);
+    [3]
+}
+//Q9/10
+function LogToConsole(TheConsole) {
+console.log("one");
+}
+TheConsole(Donkey);
